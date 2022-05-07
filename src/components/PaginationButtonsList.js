fd1724c16997120183a61c7e606680a00a66117a
DipakFarate <96985671+DipakFarate@users.noreply.github.com>
2022-05-07 10:17:18 +0530

    Add files via upload

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,9 +1,14 @@
 import React from 'react'
-const PaginationButtonsList = () => {
+// import { Post } from './Post'
 
+const PaginationButtonsList = ({ Page, handleClick }) => {
     return (
-        <div className="pagination-buttons-list">
+        <div className="col-12 d-flex justify-content-center pagination-buttons-list">
+            {     Page===1?
+                <button id={`button-${Page}`} onClick={handleClick} className="active-btn  btn btn-primary">Next</button>
 
+            :      <button id={`button-${Page}`} onClick={handleClick} className="  btn btn-primary">Next</button>
+        }
         </div>
     )
 }
